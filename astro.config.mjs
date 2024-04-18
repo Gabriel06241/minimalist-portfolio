2b3f377ed958322e53f1e1a78530437a3c67dea2
Gabriel Pérez Moreno <gabriel06241@gmail.com>
2024-04-18 01:12:04 -0500

    build changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     integrations: [],
-    site: `https://www.christophermarkus.com`,
+    site: `https://gabriel06241.github.io`,
     output: 'static',
     outDir: './docs',
+    // base: 'minimalist-portfolio-json',
     build: {
       assets: 'astro'
     }
