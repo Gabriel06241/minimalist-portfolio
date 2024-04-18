50255b23469f5b20cfc120731abb5e172b67bb98
Gabriel Pérez Moreno <gabriel06241@gmail.com>
2024-04-18 11:19:55 -0500

    build changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({
+//     integrations: [],
+//     site: `https://gabriel06241.github.io/`,
+//     output: 'static',
+//     outDir: './docs',
+//     // base: 'minimalist-portfolio-json',
+//     // base: './',
+//     build: {
+//       assets: 'astro',
+//       rollupOptions: {
+//         output: {
+//           assetFileNames: 'assets/[name][extname]',
+//         }
+//       }
+//     }
+//   });
+
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    integrations: [],
-    site: `https://gabriel06241.github.io/`,
-    output: 'static',
-    outDir: './docs',
-    // base: './',
-    build: {
-      assets: 'astro',
-      assetsPrefix: "/docs",
-    }
-  });
+export default defineConfig({});
